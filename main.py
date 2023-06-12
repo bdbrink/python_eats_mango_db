62d496072b8e9bb35a47f15fc33936e2d1649c1c
bdbrink <brendan.brink@outlook.com>
2023-06-12 17:30:39 -0500

    adding comments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
 
+# build client from local DB
 client = MongoClient("localhost", 27017)
-
+# use name defined for local db
 db = client.mydbname
-
+# field in the DB
 people = db.people
 
 people.insert_one({"name": "Josh", "age": 30})
