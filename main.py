2b5d9290310bebd5129fe78cbd377bbd5b67271d
bdbrink <brendan.brink@outlook.com>
2023-06-12 17:29:42 -0500

    adding people to db

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from pymongo import MongoClient
 
 client = MongoClient("localhost", 27017)
+
+db = client.mydbname
+
+people = db.people
+
+people.insert_one({"name": "Josh", "age": 30})
+
+for person in people.find():
+    print(person)
